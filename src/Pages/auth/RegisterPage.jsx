a9f19b6ee732b8a8a6f13c3169696f567571c46b
David Michalica <davidmichalica98@email.cz>
2022-09-27 16:39:01 +0200

    fix some bugs

--- src/Pages/auth/RegisterPage.jsx
+++ src/Pages/auth/RegisterPage.jsx
@@ -12,18 +12,6 @@ const RegistrationPage = () =>
     const userRef = useRef();
     const errRef = useRef();
 
-    const [name, setName] = useState('');
-    const [validName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
-
-    const [surname, setSurname] = useState('');
-    const [validSurname, setValidSurname] = useState(false);
-    const [surnameFocus, setSurnameFocus] = useState(false);
-
-    const [email, setEmail] = useState('');
-    const [validEmail, setValidEmail] = useState(false);
-    const [emailFocus, setEmailFocus] = useState(false);
-
     const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false);
     const [pwdFocus, setPwdFocus] = useState(false);
@@ -44,30 +32,7 @@ const RegistrationPage = () =>
     useEffect(() =>
     {
         setErrMsg('');
-    }, [surname, name, pwd, pwd, email])
-
-    useEffect(() =>
-    {
-        const result = nameRegex.test(name);
-        console.log("inputName,", name, result);
-        setValidName(result);
-    }, [name])
-
-    useEffect(() =>
-    {
-        const result = nameRegex.test(surname);
-        console.log(result);
-        console.log(surname);
-        setValidSurname(result);
-    }, [surname])
-
-    useEffect(() =>
-    {
-        const result = nameRegex.test(email);
-        console.log(result);
-        console.log(email);
-        setValidEmail(result);
-    }, [email])
+    }, [pwd, pwd])
 
     useEffect(() =>
     {
@@ -83,9 +48,6 @@ const RegistrationPage = () =>
     const divStyleClass = "flex flex-col items-baseline justify-between mt-2 max-w-lg";
     const inputStyleClass = "w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600";
     const buttonStyleClass = "px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 w-full";
-    const instructionsStyleClass = "mt-4 bg-black block text-white p-2 rounded-md";
-    const iconStyleClass = "inline-block ml-3 text-green-600";
-    const icon2StyleClass = "inline-block ml-3 text-red-600";
 
     const instr = "příjmení by mělo být minimálně 4 znaky dlouhé, mělo by začínat písmenem. Písmena, číslice, podtržítka, pomlčky jsou povolené.";
 
@@ -119,29 +81,29 @@ const RegistrationPage = () =>
                         instruction={instr}
                         userRef={userRef}
                     />
-                    <div className={divStyleClass}>
-                        <Form.Label htmlFor="registerInputPaswd" className='block mr-5'>Heslo:</Form.Label>
-                        <Form.Control
-                            type="password"
-                            id="registerInputPaswd"
-                            placeholder='zadejte heslo'
-                            className={inputStyleClass}
-                            required
-                        />
+                    <FormInput
+                        InputType='password'
+                        placeholder='zadejte heslo'
+                        htmlFor='registrationFormPwd1'
+                        inputName='Heslo:'
+                        divStyleClass={divStyleClass}
+                        regex={passwordRegex}
+                        instruction={instr}
+                        userRef={userRef}
+                    />
+                    <FormInput
+                        InputType='password'
+                        placeholder='zadejte heslo znovu'
+                        htmlFor='registrationFormPwd2'
+                        inputName='Potvrďte heslo:'
+                        divStyleClass={divStyleClass}
+                        regex={passwordRegex}
+                        instruction={instr}
+                        userRef={userRef}
+                    />
+                    <div className="flex items-baseline justify-between mb-6 mt-2">
+                        <button className={buttonStyleClass}>Registrovat se</button>
                     </div>
-                    <div className={divStyleClass}>
-                        <Form.Label htmlFor="registerInputPaswd2" className='block mr-5'>Potvrdit:</Form.Label>
-                        <Form.Control
-                            type="password"
-                            id="registerInputPaswd2"
-                            placeholder='zadejte heslo znovu'
-                            className={inputStyleClass}
-                            required
-                        />
-                    </div>
-                        <div className="flex items-baseline justify-between mb-6 mt-2">
-                            <button className={buttonStyleClass}>Registrovat se</button>
-                        </div>
                 </FormPageLayout>
             </div>
     </>

41a0053dd90f257e5bff470a3da495faf88a59fb
David Michalica <davidmichalica98@email.cz>
2022-10-14 15:11:46 +0200

    aaaaa

--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -69,27 +69,29 @@ const FormInput = (
         </span>
     </>
 
-    const instructionElement = <p
-        id={inputName + "nameNote"}
-        className={valueFocus && value && !validValue ? instructionsStyleClass : "hidden"}
-    >
-        <svg
-            className="w-5 h-5 inline mr-1"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+    const instructionElement = <>
+        <p
+            id={inputName + "nameNote"}
+            className={valueFocus && value && !validValue ? instructionsStyleClass : "hidden"}
         >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                >
-                </path>
-        </svg>
-        {instruction}
-    </p>
+            <svg
+                className="w-5 h-5 inline mr-1"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    >
+                    </path>
+            </svg>
+            {instruction}
+        </p>
+    </>
 
     return <>
         <div className={divStyle + " " + divStyleClass}>
